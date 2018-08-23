6986298f6c85d0ab69677062ea157eb63d92d17c
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-08-23 17:35:16 +0200

    num_workers to 2

--- data_accessor/model/seq2seq_forecast.py
+++ data_accessor/model/seq2seq_forecast.py
@@ -35,7 +35,7 @@ else:
     num_csku_per_query_test = 10000
     max_num_queries_train = None
     max_num_queries_test = 4
-    num_workers = 3
+    num_workers = 2
 
 if os.path.exists(label_encoder_file):
     label_encoders = load_label_encoder(label_encoder_file)
