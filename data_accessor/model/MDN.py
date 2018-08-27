37378322371fabc814692ef75f127568d0360830
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-08-27 10:50:22 +0200

    increase num gaussian to 50 and min sigma is 1

--- data_accessor/model/MDN.py
+++ data_accessor/model/MDN.py
@@ -20,7 +20,7 @@ class MDN(nn.Module):
         z_h = F.tanh(x)
         pi = F.softmax(F.sigmoid(self.z_pi(z_h)).clamp(min=0.00001), -1)
         sigma = (torch.exp(self.z_sigma(z_h))).clamp(min=1)
-        mu = (torch.exp(self.z_mu(z_h))).clamp(min=0.00001)
+        mu = ((self.z_mu(z_h)))
         if torch.sum(torch.isnan(mu)).item() or float('inf') == torch.mean(mu).item():
             print "Mu is nan for unknown reason"
             print mu
