373173ba8e2ab04c3b5a32e44c690876d97b044f
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-09-05 08:00:16 +0200

    For country 5

--- data_accessor/model/vanilla_rnn.py
+++ data_accessor/model/vanilla_rnn.py
@@ -106,9 +106,9 @@ class VanillaRNNModel(object):
             #                       exponential(sales_future[future_week_idx, :, 1:], loss_in_normal_domain),
             #                       sales_future[future_week_idx, :, 1:] + 1
             #                       )
-            loss += loss_function2(exponential(out_sales_predictions[:, 0], loss_in_normal_domain),
+            loss += loss_function2(exponential(out_sales_predictions[:, 5], loss_in_normal_domain),
                                    exponential(sales_future[future_week_idx, :, 0], loss_in_normal_domain),
-                                   sales_future[future_week_idx, :, 0] + 1
+                                   sales_future[future_week_idx, :, 5] + 1
                                    )
             # loss += loss_function(exponential(output_global_sale, loss_in_normal_domain),
             #                       exponential(global_sales[future_week_idx, :], loss_in_normal_domain)
