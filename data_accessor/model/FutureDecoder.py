7f17a418d86b83fa737a395ffc401a1ecfc0e648
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-09-13 13:38:04 +0200

    512 hs and teacher_forcing =0, 5 week forcast, loss in normal domain no weight

--- data_accessor/model/FutureDecoder.py
+++ data_accessor/model/FutureDecoder.py
@@ -30,7 +30,6 @@ class FutureDecoder(nn.Module):
         # It shares the batch_norm layer with encoder
         # (i.e., implicitly assumes encoder and decoder feature inputs have equal dimensions)
         self.batch_norm = batch_norm
-        self.relu = nn.Softplus(beta=0.8)
         self.rnn = nn.LSTM(input_size=total_num_features, hidden_size=self.hidden_size, num_layers=n_layers)
         if rnn_layer is not None:
             self.rnn.weight_ih_l0 = rnn_layer.weight_ih_l0
@@ -38,7 +37,14 @@ class FutureDecoder(nn.Module):
             self.rnn.bias_ih_l0 = rnn_layer.bias_ih_l0
             self.rnn.bias_hh_l0 = rnn_layer.bias_hh_l0
 
-        self.out_sale = nn.Linear(self.hidden_size + NUM_COUNTRIES + 1, num_output)
+        self.out_sale_ch1 = nn.Sequential(nn.Linear(self.hidden_size + NUM_COUNTRIES + 1, hidden_size), nn.Dropout(0.7),
+                                          nn.Softplus())
+        self.out_sale_ch2 = nn.Sequential(nn.Linear(self.hidden_size + NUM_COUNTRIES + 1, hidden_size), nn.Dropout(0.7),
+                                          nn.Softplus())
+        self.out_sale = nn.Sequential(
+            nn.Linear(self.hidden_size * 2, num_output),
+            nn.Softplus()
+        )
 
     def forward(self, input, hidden, embedded_inputs, encoder_outputs=None,
                 ):
@@ -50,12 +56,16 @@ class FutureDecoder(nn.Module):
         # BATCH_SIZE x TOTAL_NUM_FEAT
         features = self.batch_norm(torch.cat(numeric_features + embedded_inputs, dim=1))
         output, hidden = self.rnn(features.unsqueeze(0), hidden)
-        out_sales_prediction = self.relu(self.out_sale(
-            torch.cat(
-                [output[0],
-                 input[:, feature_indices[STOCK]].float(),
-                 input[:, feature_indices[DISCOUNT_MATRIX]].float()
-                 ], dim=1))).squeeze()  # (BATCH_SIZE,NUM_OUTPUT)
+        encoded_features = torch.cat(
+            [output[0],
+             input[:, feature_indices[STOCK]].float(),
+             input[:, feature_indices[DISCOUNT_MATRIX]].float()
+             ], dim=1)
+
+        # (BATCH_SIZE,NUM_OUTPUT)
+        out_sales_prediction = self.out_sale(
+            torch.cat([self.out_sale_ch1(encoded_features), self.out_sale_ch2(encoded_features)])
+        ).squeeze()
         if len(out_sales_prediction.shape) == 1 and self.num_output > 1:
             out_sales_prediction = out_sales_prediction[None, :]
         out_global_sales = log(torch.sum(exponential(out_sales_prediction, IS_LOG_TRANSFORM), dim=1), IS_LOG_TRANSFORM)
