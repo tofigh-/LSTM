4505d77cdc578e210536d33326c507dd53ab033d
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-09-18 18:11:14 +0200

    No batch-norm

--- data_accessor/model/EncoderRNN.py
+++ data_accessor/model/EncoderRNN.py
@@ -36,7 +36,7 @@ class EncoderRNN(nn.Module):
 
         # It turns out I'm not normalizing, I'm transforming. That was a surprise. What happened to BatchNorm1d!?
         self.batch_norm = nn.Sequential(
-            # nn.BatchNorm1d(total_num_features),
+            nn.BatchNorm1d(total_num_features),
             nn.Linear(in_features=total_num_features, out_features=total_num_features),
             nn.Dropout(rnn_dropout)
         )
