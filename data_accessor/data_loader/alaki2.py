ba4421d67647e84123adbb4ae325b7ef194d998d
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-09-20 23:51:41 +0200

    Layer-norm off with time-discounted loss

--- data_accessor/data_loader/alaki2.py
+++ data_accessor/data_loader/alaki2.py
@@ -1,13 +1,9 @@
-import sqlite3
-db_path = "/Users/tnaghibi/Downloads/accuracy_metrics_201645.db"
-conn = sqlite3.connect(db_path)
-con_curs = conn.cursor()
-sibr_obs = "sibr_obs"
-sibr_fc = "sibr_fc"
-black_price = "black_price"
-risk_date = 'risk_discounting_start_date'
-end_season = 'season_end_date'
-csku_id = 'csku'
-query = "Select sum(abs(sibr_fc-sibr_obs)*black_price) / sum(sibr_obs * black_price) from BT_ACCURACY_METRICS where week=45 and country=0agit "
-con_curs.execute(query)
-print con_curs.fetchall()
+import numpy as np
+
+t = np.arange(1,5)
+ys = np.array([12 * t**2.5,14 * t **3.12]).T
+def fit(x,a,m): # power-law fit (based on previous studies)
+    return a*(x**m)
+a1,a2= np.linalg.lstsq(np.vstack([np.ones(len(ys)), np.log(t)]).T, np.log(ys))[0] # calculating fitting coefficients (a,m)
+y_predict = fit(np.repeat(np.arange(1,5)[:,None],2,axis=1),np.exp(a1),a2) # prediction based of fitted model
+print y_predict - ys
