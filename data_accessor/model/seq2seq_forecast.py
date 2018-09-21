3d1704dd775e767df8824bb65542be18e507b869
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-09-21 16:17:58 +0200

    Layer-norm off with time-discounted loss

--- data_accessor/model/seq2seq_forecast.py
+++ data_accessor/model/seq2seq_forecast.py
@@ -34,7 +34,7 @@ if debug_mode:
     max_num_queries_train = 1
     max_num_queries_test = 1
 else:
-    num_csku_per_query_train = 10000
+    num_csku_per_query_train = 2500
     num_csku_per_query_test = 10000
     max_num_queries_train = None
     max_num_queries_test = 5
