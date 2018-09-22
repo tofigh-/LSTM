31e31cf278e719fd9eef915eaa425ac77ee6e128
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-09-22 11:54:33 +0200

    Layer-norm and log_normal all_weeks draw from log-normal

--- data_accessor/model/EncoderRNN.py
+++ data_accessor/model/EncoderRNN.py
@@ -37,7 +37,7 @@ class EncoderRNN(nn.Module):
         # It turns out I'm not normalizing, I'm transforming. That was a surprise. What happened to BatchNorm1d!?
         self.batch_norm = nn.Sequential(nn.LayerNorm(total_num_features),
                                         nn.Linear(in_features=total_num_features, out_features=total_num_features),
-                                        nn.Dropout(rnn_dropout)
+                                        # nn.Dropout(rnn_dropout)
                                         )
         self.time_dist_batch_norm = TimeDistributed(self.batch_norm)
 
