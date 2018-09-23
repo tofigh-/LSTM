996b3456ab633e2a20053ec326bd24447027767a
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-09-23 23:54:17 +0200

    log-normal-first week prediction

--- data_accessor/model/vanilla_rnn.py
+++ data_accessor/model/vanilla_rnn.py
@@ -125,9 +125,6 @@ class VanillaRNNModel(object):
             loss += loss_function(out_sales_mean_predictions, out_sales_variance_predictions,
                                   sales_future[future_week_idx])
 
-            loss += loss_function2(exponential(output_global_sale, IS_LOG_TRANSFORM),
-                                   exponential(global_sales[future_week_idx, :], IS_LOG_TRANSFORM)
-                                   )
             if use_teacher_forcing:
                 future_unknown_estimates = sales_future.data[future_week_idx, :, :]
             else:
