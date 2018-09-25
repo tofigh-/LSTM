1c5f66882de52623c12f1189f44f19177fa4e15a
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-09-25 13:58:47 +0200

    fix future_bug, LN with dropout for decoder

--- data_accessor/data_loader/Settings.py
+++ data_accessor/data_loader/Settings.py
@@ -86,7 +86,7 @@ ENCODER_WEIGHT_DECAY = 0.00001
 TEST_STOCK_THRESHOLD = 5
 TRAIN_STOCK_THRESHOLD = 2
 use_future_unknown_estimates = True
-USE_SAMPLE_RATIO = 0.3
+USE_SAMPLE_RATIO = 1.0
 to_print_variables = ['TRAIN_STOCK_THRESHOLD', 'TEST_STOCK_THRESHOLD', 'use_future_unknown_estimates', 'RESUME',
                       'SIZE_AVERAGE','USE_SAMPLE_RATIO',
                       'SUM_WEIGHT', 'ENCODER_WEIGHT_DECAY', 'RNN_DROPOUT', 'BI_DIRECTIONAL', 'NUM_BATCH_SAVING_MODEL',
