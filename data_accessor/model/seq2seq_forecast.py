211e37e8384fd9b653140632ecc020193fbde35f
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-10-12 15:40:27 +0200

    Stock threshold on test is 0, batch_size 32, 25% zero sales

--- data_accessor/model/seq2seq_forecast.py
+++ data_accessor/model/seq2seq_forecast.py
@@ -66,7 +66,7 @@ train_transform = Transform(
     keep_zero_stock_filter=0.0,
     keep_percentage_zero_price=0.0,
     stock_threshold=TRAIN_STOCK_THRESHOLD,
-    keep_zero_sale_filter=ZERO_SALE_PERCENTAGE,
+    keep_zero_sale_filter=TRAIN_ZERO_SALE_PERCENTAGE,
     activate_filters=True)
 
 if label_encoders is None:
@@ -84,7 +84,7 @@ test_transform = Transform(
     keep_zero_stock_filter=0.0,
     keep_percentage_zero_price=0.0,
     stock_threshold=TEST_STOCK_THRESHOLD,
-    keep_zero_sale_filter=1.0,
+    keep_zero_sale_filter=TEST_ZERO_SALE_PERCENTAGE,
     activate_filters=True)
 
 train_db = DatasetReader(
