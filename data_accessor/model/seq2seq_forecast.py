21965738738749ee7fcfeecb086e6e64b8f782f6
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-10-12 09:55:18 +0200

    Stock threshold on test is 0, batch_size 32, 25% zero sales

--- data_accessor/model/seq2seq_forecast.py
+++ data_accessor/model/seq2seq_forecast.py
@@ -84,7 +84,7 @@ test_transform = Transform(
     keep_zero_stock_filter=0.0,
     keep_percentage_zero_price=0.0,
     stock_threshold=TEST_STOCK_THRESHOLD,
-    keep_zero_sale_filter=1.0,
+    keep_zero_sale_filter=ZERO_SALE_PERCENTAGE,
     activate_filters=True)
 
 train_db = DatasetReader(
