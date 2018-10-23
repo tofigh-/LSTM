8d0844a009e14cda01e2106b36caece2b7d8b229
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-10-23 23:09:09 +0200

    Stock threshold on test is 0, batch_size 32, 25% zero sales

--- data_accessor/model/seq2seq_forecast.py
+++ data_accessor/model/seq2seq_forecast.py
@@ -150,7 +150,7 @@ print "d_model is: " + str(d_model)
 attention_model = cuda_converter(make_model(embedding_descriptions=embedding_descripts,
                                             total_input=TOTAL_INPUT,
                                             forecast_length=OUTPUT_SIZE,
-                                            N=10,
+                                            N=6,
                                             d_model=d_model,
                                             d_ff=4 * d_model,
                                             h=14,
