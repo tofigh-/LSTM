8082e9d33f9c058afeacf747953360251cf957d8
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-10-23 16:04:12 +0200

    Stock threshold on test is 0, batch_size 32, 25% zero sales

--- data_accessor/model/cache_data.py
+++ data_accessor/model/cache_data.py
@@ -5,10 +5,14 @@ from data_accessor.data_loader.utilities import append_lists
 from data_accessor.data_loader.Settings import *
 from time import time
 from random import shuffle, seed
+import os
 
 
 def cache_data(dataset):
     extension = 'npy'
+    dir_path = 'cached_data'
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
 
     def save_batches(all_losses, all_batches, batch_num):
         st = time()
@@ -16,16 +20,19 @@ def cache_data(dataset):
         shuffle(all_batches)
         seed(42)
         shuffle(all_losses)
-        output_path_batch = "chuck_saved_{seq_length}_{output_size}_{batch_num}_batch.{extension}".format(
-            batch_num=batch_num,
-            seq_length=TOTAL_INPUT,
-            output_size=OUTPUT_SIZE,
-            extension=extension)
-        output_path_loss = "chuck_saved_{seq_length}_{output_size}_{batch_num}_loss.{extension}".format(
-            batch_num=batch_num,
-            seq_length=TOTAL_INPUT,
-            output_size=OUTPUT_SIZE,
-            extension=extension)
+        output_path_batch = os.path.join(dir_path,
+                                         "chuck_saved_{seq_length}_{output_size}_{batch_num}_batch.{extension}".format(
+                                             batch_num=batch_num,
+                                             seq_length=TOTAL_INPUT,
+                                             output_size=OUTPUT_SIZE,
+                                             extension=extension))
+        output_path_loss = os.path.join(dir_path,
+                                        "chuck_saved_{seq_length}_{output_size}_{batch_num}_loss.{extension}".format(
+                                            batch_num=batch_num,
+                                            seq_length=TOTAL_INPUT,
+                                            output_size=OUTPUT_SIZE,
+                                            extension=extension)
+                                        )
         save(np.concatenate(all_batches), output_path_batch)
         print ("time to save batch {time_amount} sec".format(time_amount=time() - st))
         save(np.concatenate(all_losses), output_path_loss)
