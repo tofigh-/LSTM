a5a8ca1f1c89bcba3ba1680c654fa3b2ec60a8ae
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-10-23 23:03:00 +0200

    Stock threshold on test is 0, batch_size 32, 25% zero sales

--- data_accessor/data_loader/file_based_dataset.py
+++ data_accessor/data_loader/file_based_dataset.py
@@ -8,6 +8,7 @@ import os
 from subprocess import Popen, PIPE
 from os.path import join
 from random import shuffle, seed
+import signal
 
 
 class FileBasedDatasetReader(Dataset):
@@ -20,13 +21,13 @@ class FileBasedDatasetReader(Dataset):
         self.temp_path = '/home/tnaghibi/cached_data'
         if not os.path.exists(self.temp_path):
             os.mkdir(self.temp_path)
-        process_batch = Popen(['exec', 'cp', join(path_to_training_dir, self.all_batch_files[0]), self.temp_path],
-                              stdout=PIPE, shell=True)
-        process_loss = Popen(['exec', 'cp', join(path_to_training_dir, self.all_loss_files[0]), self.temp_path],
-                             stdout=PIPE, shell=True)
+        process_batch = Popen(['cp', join(path_to_training_dir, self.all_batch_files[0]), self.temp_path],
+                              stdout=PIPE, shell=True, preexec_fn=os.setsid)
+        process_loss = Popen(['cp', join(path_to_training_dir, self.all_loss_files[0]), self.temp_path],
+                             stdout=PIPE, shell=True, preexec_fn=os.setsid)
         process_batch.wait()
-        process_batch.kill()
-        process_loss.kill()
+        self._kill_process(process_batch)
+        self._kill_process(process_loss)
         self.p_batch = None
         self.p_loss = None
         self.p_rm_batch = None
@@ -34,6 +35,12 @@ class FileBasedDatasetReader(Dataset):
 
     def __len__(self):
         return self.length
+
+    def _kill_process(self, process):
+        try:
+            os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+        except:
+            print "no process found to kill"
 
     def reset_length(self, length):
         self.length = length
@@ -46,32 +53,30 @@ class FileBasedDatasetReader(Dataset):
         shuffle(self.all_loss_files)
 
     def __getitem__(self, idx):
-        if self.p_batch is not None:
-            self.p_batch.kill()
-            self.p_loss.kill()
-        if self.p_rm_batch is not None:
-            self.p_rm_batch.kill()
-            self.p_rm_loss.kill()
+        self._kill_process(self.p_batch)
+        self._kill_process(self.p_loss)
+        self._kill_process(self.p_rm_batch)
+        self._kill_process(self.p_rm_loss)
         self.p_batch = Popen(
-            ['exec', 'cp', join(self.path_to_training_dir, self.all_batch_files[(idx + 1) % self.length]), self.temp_path],
-            stdout=PIPE, shell=True)
+            ['cp', join(self.path_to_training_dir, self.all_batch_files[(idx + 1) % self.length]), self.temp_path],
+            stdout=PIPE, shell=True, preexec_fn=os.setsid)
         self.p_loss = Popen(
-            ['exec', 'cp', join(self.path_to_training_dir, self.all_loss_files[(idx + 1) % self.length]), self.temp_path],
-            stdout=PIPE, shell=True)
+            ['cp', join(self.path_to_training_dir, self.all_loss_files[(idx + 1) % self.length]), self.temp_path],
+            stdout=PIPE, shell=True, preexec_fn=os.setsid)
         if not os.path.exists(join(self.temp_path, self.all_batch_files[idx])):
             p_batch = Popen(
-                ['exec', 'cp', join(self.path_to_training_dir, self.all_batch_files[(idx) % self.length]),
-                 self.temp_path], stdout=PIPE, shell=True)
+                ['cp', join(self.path_to_training_dir, self.all_batch_files[(idx) % self.length]),
+                 self.temp_path], stdout=PIPE, shell=True, preexec_fn=os.setsid)
             p_loss = Popen(
-                ['exec', 'cp', join(self.path_to_training_dir, self.all_loss_files[(idx) % self.length]),
-                 self.temp_path], stdout=PIPE, shell=True)
+                ['cp', join(self.path_to_training_dir, self.all_loss_files[(idx) % self.length]),
+                 self.temp_path], stdout=PIPE, shell=True, preexec_fn=os.setsid)
             p_batch.wait()
-            p_batch.kill()
-            p_loss.kill()
+            self._kill_process(p_batch)
+            self._kill_process(p_loss)
 
         batch_data = np.load(join(self.temp_path, self.all_batch_files[idx]))
         loss_masks = np.load(join(self.temp_path, self.all_loss_files[idx]))
 
-        self.p_rm_batch = Popen(['exec rm', join(self.temp_path, self.all_batch_files[idx])])
-        self.p_rm_loss = Popen(['exec rm', join(self.temp_path, self.all_loss_files[idx])])
+        self.p_rm_batch = Popen(['rm', join(self.temp_path, self.all_batch_files[idx])])
+        self.p_rm_loss = Popen(['rm', join(self.temp_path, self.all_loss_files[idx])])
         return zip(batch_data, loss_masks)
