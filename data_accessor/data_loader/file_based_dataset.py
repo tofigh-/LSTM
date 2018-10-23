4f76ddfe454b2eb54835e817c0a429a68ddc8afc
Tofigh Naghibi <tofigh.naghibi@zalando.de>
2018-10-23 20:55:04 +0200

    Stock threshold on test is 0, batch_size 32, 25% zero sales

--- data_accessor/data_loader/file_based_dataset.py
+++ data_accessor/data_loader/file_based_dataset.py
@@ -5,6 +5,8 @@ from random import shuffle
 import numpy as np
 from torch.utils.data import Dataset
 import os
+from subprocess import Popen
+from os.path import join
 
 
 class FileBasedDatasetReader(Dataset):
@@ -14,6 +16,11 @@ class FileBasedDatasetReader(Dataset):
         self.all_batch_files = sorted(filter(lambda x: x.endswith('_batch.npy'), os.listdir(self.path_to_training_dir)))
         self.all_loss_files = sorted(filter(lambda x: x.endswith('_loss.npy'), os.listdir(self.path_to_training_dir)))
         self.length = len(self.all_batch_files)
+        self.temp_path = '/home/tnaghibi/cached_data'
+        if not os.path.exists(self.temp_path):
+            os.mkdir(self.temp_path)
+        Popen(['cp', join(path_to_training_dir, self.all_batch_files[0]), self.temp_path])
+        Popen(['cp', join(path_to_training_dir, self.all_loss_files[0]), self.temp_path])
 
     def __len__(self):
         return self.length
@@ -25,6 +32,10 @@ class FileBasedDatasetReader(Dataset):
         return
 
     def __getitem__(self, idx):
-        batch_data = np.load(os.path.join(self.path_to_training_dir, self.all_batch_files[idx]))
-        loss_masks = np.load(os.path.join(self.path_to_training_dir, self.all_loss_files[idx]))
+        Popen(['cp', join(self.path_to_training_dir, self.all_batch_files[(idx+1) % self.length]), self.temp_path])
+        Popen(['cp', join(self.path_to_training_dir, self.all_loss_files[(idx+1) % self.length]), self.temp_path])
+        batch_data = np.load(os.path.join(self.temp_path, self.all_batch_files[idx]))
+        loss_masks = np.load(os.path.join(self.temp_path, self.all_loss_files[idx]))
+        Popen(['rm', join(self.temp_path, self.all_batch_files[idx])])
+        Popen(['rm', join(self.temp_path, self.all_loss_files[idx])])
         return zip(batch_data, loss_masks)
